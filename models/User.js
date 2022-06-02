37274f6b614efa16d8ab8883cd88d26f0b6212a3
Andy Nguyen <andynguyen_3@hotmail.com>
2022-06-02 19:51:16 +1000

    deleted routes folder. modified models

--- models/User.js
+++ models/User.js
@@ -6,26 +6,23 @@ class User extends Model {}
 User.init(
     {
         id: {
-            type: DataTypes.INTERGER,
+            type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
         },
 
-        firstName: {
+        user_firstName: {
             type: DataTypes.STRING,
-        },
-
-        lastName: {
-            type: DataTypes.STRING,
-        },
-
-        contactNo: {
-            type: DataTypes.INTERGER,
             allowNull: false,
         },
 
-        email: {
+        user_lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        user_email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: {
@@ -34,15 +31,46 @@ User.init(
             },
         },
 
-        myWorkouts: {
+        user_postcode: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        user_gender: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+        },
+
+        user_age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        user_bio: {
             type: DataTypes.STRING,
+        },
+
+        user_password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        workout_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "workout",
+                key: "id",
+            },
         },
     },
 
     {
         sequelize,
+        timestamps: false,
+        freezeTableName: true,
         modelName: 'User'
-    }
+    },
 );
 
 module.exports = User;

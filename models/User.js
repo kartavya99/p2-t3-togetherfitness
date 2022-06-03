6f765c2be36aa10c303f6658a9c2f3bc2a74e8d9
kartavya <kartavyas@gmail.com>
2022-06-03 15:04:05 +1000

    updated files and models

--- models/User.js
+++ models/User.js
@@ -1,76 +1,68 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class User extends Model {}
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-
-        user_firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
-        user_lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
-        user_email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: {
-                args: true,
-                message: "This email is already in use."
-            },
-        },
-
-        user_postcode: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-
-        user_gender: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true,
-        },
-
-        user_age: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-
-        user_bio: {
-            type: DataTypes.STRING,
-        },
-
-        user_password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
-        workout_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "workout",
-                key: "id",
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
 
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        modelName: 'User'
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
+
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        message: "This email is already in use.",
+      },
+    },
+
+    // password: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+
+    // user_postcode: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false,
+    // },
+
+    // user_gender: {
+    //     type: DataTypes.BOOLEAN,
+    //     allowNull: false,
+    //     defaultValue: true,
+    // },
+
+    // user_age: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false,
+    // },
+
+    // user_bio: {
+    //     type: DataTypes.STRING,
+    // },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "user",
+  }
 );
 
 module.exports = User;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,47 +1,48 @@
-const { User } = require('../models');
+// const { User } = require("../models");
 
-const userData = [
-    {
-        user_firstName: 'Billy',
-        user_lastName: 'Slater',
-        user_contact: 11111110,
-        user_email: 'billyslater@gmail.com',
-        workouts: 'Weights',
-    },
+// const userData =
+[
+  {
+    firstName: "Billy",
+    lastName: "Slater",
+    // contact: 11111110,
+    email: "billyslater@gmail.com",
+    // workouts: 'Weights',
+  },
 
-    {
-        user_firstName: 'Michael',
-        user_lastName: 'Scott',
-        user_contact: 11111111,
-        user_email: 'michaelscott@gmail.com',
-        workouts: 'Run',
-    },
+  {
+    firstName: "Michael",
+    lastName: "Scott",
+    // contact: 11111111,
+    email: "michaelscott@gmail.com",
+    // workouts: 'Run',
+  },
 
-    {
-        user_firstName: 'Mike',
-        user_lastName: 'Trout',
-        user_contact: 11111112,
-        user_email: 'miketrout@gmail.com',
-        workouts: 'Cycle',
-    },
+  {
+    firstName: "Mike",
+    lastName: "Trout",
+    // contact: 11111112,
+    email: "miketrout@gmail.com",
+    // workouts: 'Cycle',
+  },
 
-    {
-        user_firstName: 'Tom',
-        user_lastName: 'Brady',
-        user_contact: 11111113,
-        user_email: 'tombrady@gmail.com',
-        workouts: 'Yoga',
-    },
+  {
+    firstName: "Tom",
+    lastName: "Brady",
+    // contact: 11111113,
+    email: "tombrady@gmail.com",
+    // workouts: 'Yoga',
+  },
 
-    {
-        user_firstName: 'Michael',
-        user_lastName: 'Jordan',
-        user_contact: 11111114,
-        user_email: 'michaeljordan@gmail.com',
-        workouts: 'Walk',
-    },
+  {
+    firstName: "Michael",
+    lastName: "Jordan",
+    // contact: 11111114,
+    email: "michaeljordan@gmail.com",
+    // workouts: 'Walk',
+  },
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+// const seedUsers = () => User.bulkCreate(userData);
 
-module.exports = seedUsers;
+// module.exports = seedUsers;
